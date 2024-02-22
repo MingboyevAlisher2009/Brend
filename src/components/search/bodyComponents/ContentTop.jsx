21c56b478c16d61edbabf64ef7b01dd96121dc5a
FixPlayer <nikwilde888@gmail.com>
2024-02-22 11:43:47 +0500

    editing

--- src/components/search/bodyComponents/ContentTop.jsx
+++ src/components/search/bodyComponents/ContentTop.jsx
@@ -17,7 +17,7 @@ function ContentTop() {
                             </select>
                         </div>
                         <div className="flex flex-row p-one absolute right-4 top-cent border border-1 bg-white rounded-md outline-gray-300">
-                            <img className="bg-white" src={blockView} alt="" />
+                            <img className="bg-white " src={blockView} alt="" />
                             <img className="bg-gray-300 ml-4 border border-l-1" src={listView} alt="" />
                         </div>
                     </div>
